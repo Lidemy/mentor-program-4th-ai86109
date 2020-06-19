05ec81e337842677a0d406064d51cf98b9c8a1a8
Derek Yen <cyen@Derekde-MacBook-Pro.local>
2020-06-19 23:55:49 +0800

    wk2 finished

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,15 @@
 function capitalize(str) {
-  
+    var strCharCode = str.charCodeAt(0)
+    if(strCharCode >= 97 && strCharCode <= 122){
+        var ans = ''
+        ans += str[0].toUpperCase()
+        for(let i=1; i<str.length; i++){
+            ans += str[i]
+        }
+        return ans
+    } else {
+        return str
+    }
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,19 @@
 function join(arr, concatStr) {
-  
+    var joinStr = ''
+    joinStr += arr[0]
+    for(let i=1; i<arr.length; i++){
+        joinStr += concatStr + arr[i]
+    }
+    return joinStr
 }
 
 function repeat(str, times) {
-  
+    var repeatStr = ''
+    for(let i=0; i<times; i++){
+        repeatStr += str
+    }
+    return repeatStr
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(["a", 1, "b", 2, "c", 3], ','));
+console.log(repeat('yoyo', 2));
